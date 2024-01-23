c44e8589702d38111255c0bb59a1d32228f34a5d
dell <dell@DESKTOP-UL67J8E>
2024-01-23 13:58:24 +0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class  Student
+    {
+        public string sname;
+        public float matricMarks;
+        public float fscMarks;
+        public float ecatMarks;
+        public float aggregate;
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student s1 = new Student();
+            Student s2 = new Student();
+            s1.sname = "Mobeen";
+            s2.sname = "Butt";
+            Console.WriteLine(s1.sname);
+            Console.WriteLine(s2.sname);
+
+            Console.Read();
+
+        }
+    }
+}
